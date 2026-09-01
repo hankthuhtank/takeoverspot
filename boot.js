74db3c675a5f15c7292a3462585065521b85a960
hankthuhtank <safihelal@gmail.com>
2026-09-01 14:13:05 -0500

    Switch TAKEOVER to unified V3 app

--- boot.js
+++ boot.js
@@ -1,5 +1,4 @@
 (()=>{'use strict';
-const css=document.createElement('link');css.rel='stylesheet';css.href='polish.css?v=1';document.head.appendChild(css);
-const launchCss=document.createElement('link');launchCss.rel='stylesheet';launchCss.href='launch.css?v=4';document.head.appendChild(launchCss);
-const polish=document.createElement('script');polish.src='polish.js?v=2';polish.onload=()=>{const launch=document.createElement('script');launch.src='launch.js?v=4';launch.onload=()=>{const main=document.createElement('script');main.src='takeover-v2.js?v=4';document.body.appendChild(main)};document.body.appendChild(launch)};document.body.appendChild(polish);
+const css=document.createElement('link');css.rel='stylesheet';css.href='takeover-v3.css?v=1';document.head.appendChild(css);
+const main=document.createElement('script');main.src='takeover-v3.js?v=1';document.body.appendChild(main);
 })();
